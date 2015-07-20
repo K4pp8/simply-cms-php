7ba57606080f8c35b7951128ec4d4e6145715571
K4pp8 <k.bojaxhiu@gmail.com>
2015-07-20 23:54:18 +0200

    Add page logic

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -3,5 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title> Cms </title>
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/css/style.css">
 </head>
 <body>
+	<div class="wrapper">
+		<h1> My CMs</h1>
