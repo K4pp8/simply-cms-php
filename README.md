6f135c8573437992cba957b8655174d08e198009
K4pp8 <k.bojaxhiu@gmail.com>
2015-07-21 09:47:55 +0200

    Aggiunto supporto a bootstrap

--- README.md
+++ README.md
@@ -6,7 +6,5 @@ Da fixare:
 - .htacces ( problemi con sintassi o/e apache )
 
 Da implementare: 
-- conessione al db
-- gestione pagine
 - login
 - bootstrap
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,10 +1,36 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title> Cms </title>
-	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/css/style.css">
-</head>
-<body>
-	<div class="wrapper">
-		<h1> My CMS</h1>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/css/style.css">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+     <!-- Begin page content -->
+    <div class="container">
+      <div class="page-header">
+        <h1>my CMS</h1>
+      </div>
+      
+    
+
+	
